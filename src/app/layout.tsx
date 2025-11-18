0220667cbd2206f3a5283b8190e1260139063a7a
Amit Mhaske <itsthelucifer@gmail.com>
2025-11-18 15:24:26 +0000

    When I select the theme from the icon which is present near the bell ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="deep-night"
-            enableSystem={false}
-            disableTransitionOnChange
-        >
+        <ThemeProvider>
             {children}
             <Toaster />
         </ThemeProvider>
